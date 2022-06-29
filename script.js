5c6bbd7b3b54574fe5a05fb1ca054ee7ee7fc173
Naim-Ahmed-Shuvo <shuvoniam123@gmail.com>
2022-06-29 09:25:28 +0600

    Uploading project folder

---
+++ script.js
@@ -0,0 +1,17 @@
+let color = document.getElementById("color");
+
+color.parentElement.style.cursor="pointer"
+
+function generateColor(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+
+}
+
+function copyColor(){
+    navigator.clipboard.writeText(color.innerHTML).then(()=> alert('Done!'));
+    // alert("copied! ", color.innerHTML)
+}
+
+setInterval(generateColor,2000)
